b6f2ee322b7441b93f01e81e7a2fef1c9ef85e1d
Nate Birkholz <nbirkholz@gmail.com>
2020-06-09 11:54:23 -0600

    Initial Commit

---
+++ InventoryApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InventoryApp
+//
+//  Created by Nathan Birkholz on 6/9/20.
+//  Copyright © 2020 Nathan Birkholz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
